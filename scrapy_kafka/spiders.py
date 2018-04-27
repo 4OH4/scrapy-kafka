b66c4722b71bc7c8e33b7b78f117e127d70d902d
Akshay Philar <akshayphilar@gmail.com>
2018-04-27 15:05:03 +0530

    replacing SimpleConsumer with KafkaConsumer

--- scrapy_kafka/spiders.py
+++ scrapy_kafka/spiders.py
@@ -2,9 +2,8 @@
 from scrapy import signals
 from scrapy.exceptions import DontCloseSpider
 from scrapy.spiders import Spider
-
-from kafka.client import KafkaClient
-from kafka.consumer import SimpleConsumer
+from scrapy.http import HtmlResponse
+from kafka.consumer import KafkaConsumer
 
 
 class KafkaSpiderMixin(object):
@@ -29,7 +28,7 @@ class KafkaSpiderMixin(object):
 
         return message.message.value
 
-    def setup_kafka(self, settings):
+    def setup_kafka(self, settings, **configs):
         """Setup redis connection and idle signal.
 
         This should be called after the spider has set its crawler object.
@@ -42,15 +41,16 @@ class KafkaSpiderMixin(object):
 
         hosts = settings.get('SCRAPY_KAFKA_HOSTS', ['localhost:9092'])
         consumer_group = settings.get('SCRAPY_KAFKA_SPIDER_CONSUMER_GROUP', 'scrapy-kafka')
-        _kafka = KafkaClient(hosts)
+
         # wait at most 1sec for more messages. Otherwise continue
-        self.consumer = SimpleConsumer(_kafka, consumer_group, self.topic,
-                                       auto_commit=True, iter_timeout=1.0)
+        self.consumer = KafkaConsumer(self.topic, group_id=consumer_group,
+                                      bootstrap_servers=hosts, auto_offset_reset='earliest',
+                                      enable_auto_commit=False, iter_timeout=1.0, **configs)
         # idle signal is called when the spider has no requests left,
         # that's when we will schedule new requests from kafka topic
         self.crawler.signals.connect(self.spider_idle, signal=signals.spider_idle)
         self.crawler.signals.connect(self.item_scraped, signal=signals.item_scraped)
-        self.log("Reading URLs from kafka topic '%s'" % self.kafka_topic)
+        self.log("Reading messages from kafka topic '%s'" % self.kafka_topic)
 
     def next_request(self):
         """
@@ -58,17 +58,17 @@ class KafkaSpiderMixin(object):
 
         :rtype: str or None
         """
-        message = self.consumer.get_message(True)
-        url = self.process_kafka_message(message)
-        if not url:
-            return None
-        return self.make_requests_from_url(url)
+        NotImplemented
 
     def schedule_next_request(self):
         """Schedules a request if available"""
-        req = self.next_request()
-        if req:
-            self.crawler.engine.crawl(req, spider=self)
+        try:
+            message = self.consumer.next()
+            url, html_body = self.process_kafka_message(message)
+            if html_body:
+                self.parse(HtmlResponse(url=url, body=html_body))
+        except StopIteration:
+            self.log("No messages in kafka")
 
     def spider_idle(self):
         """Schedules a request if available, otherwise waits."""
